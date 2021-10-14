7741c12f88292edcf83475dce525abd73512aac5
Natalia Sanchez <natalia-sanchezc@hotmail.com>
2021-10-14 17:00:30 +1100

    more tidyverse

--- Workshop3.R
+++ Workshop3.R
@@ -15,6 +15,13 @@ survey%>%head()
 survey_USD<-survey%>%
   filter(currency=="USD")
 
+#select observations with annual salary above or equal to 50K using USD data
+
+survey_USD%>%
+  filter(
+    annual_salary>="50000"
+  )
+
 #dataset with USD and Females
 
 survey_USD_Female<-survey%>%
@@ -35,3 +42,38 @@ survey_AUD_Female<-survey%>%
 
 survey%>%select(timestamp, gender, currency)
 
+#arrange function
+
+survey_USD%>%arrange(annual_salary)
+
+#function to have a look an specific info inside a column (old one)
+
+min(survey_USD$annual_salary)
+
+#what is the earliest date for the survey
+
+min(survey_USD$timestamp)
+
+#what is the latest date for the survey
+max(survey_USD$timestamp)
+
+#select the observations with annual salary grater than 0 and save it under the same name
+
+survey_USD<-survey%>%
+  filter(
+    annual_salary > 0
+  )
+
+#Sort in a descending order
+
+survey_USD%>%arrange(
+  desc(annual_salary)
+  )
+
+#what are industries in the data set
+
+survey_USD%>%distinct(industry)
+
+# how many under each industry
+
+survey_USD%>%count(industry,sort=TRUE)
